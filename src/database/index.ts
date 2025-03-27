4eb3b66a58a08bcee520c2ec3b78f0d942c331ff
Orkuma Ivo <orkumaivo@Orkumas-MBP-2.broadband>
2025-03-27 19:11:58 +0000

    Add movie service with caching, update GraphQL schema and resolvers

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,4 +5,19 @@ import { UserModel } from './db-models/user'
 
 const sql = sequelize
 
+UserModel.hasMany(EndingModel, {
+  foreignKey: 'userId',
+  as: 'endings',
+})
+
+ReviewModel.belongsTo(UserModel, {
+  foreignKey: 'userId',
+  as: 'user',
+})
+
+ReviewModel.belongsTo(EndingModel, {
+  foreignKey: 'endingId',
+  as: 'ending',
+})
+
 export { sql, ReviewModel, EndingModel, UserModel }
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -5,8 +5,44 @@ export const typeDefs = `
     content: String
     aiName: String
   }
+
+  type Rating {
+    Source: String
+    Value: String
+  }
+
+  type Movie {
+    Title: String,
+    Year: String,
+    Rated: String,
+    Released: String,
+    Runtime: String,
+    Genre: String,
+    Director: String,
+    Writer: String,
+    Actors: String,
+    Plot: String,
+    Language: String,
+    Country: String,
+    Awards: String,
+    Poster: String,
+    Ratings: [Rating],
+    Metascore: String,
+    imdbRating: String,
+    imdbVotes: String,
+    imdbID: String,
+    Type: String,
+    DVD: String,
+    BoxOffice: String,
+    Production: String,
+    Website: String,
+    Response: String
+  }
+    
   type Query {
     endings: [Ending]
+    movieByName(name: String): Movie
+    movieById(imdbId: String): Movie
   }
 
   type Mutation {
